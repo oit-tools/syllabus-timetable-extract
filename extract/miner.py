4bb55b713033c04fef0bb30a0cb4a9b295b1e542
kota <52403688+yashikota@users.noreply.github.com>
2022-09-01 10:31:29 +0900

    update: pathの追加

--- extract/miner.py
+++ extract/miner.py
@@ -15,12 +15,11 @@ def pdf(year: str):
 
     for file in tqdm(files):
         # テキストの抽出
-        text = extract_text(f"./data/{year}/pdf/{file}")
+        text = extract_text(f"{path}/{file}")
 
         # 英数字6桁を抽出後、末尾にA0を付加
         if file[0] == "X":
             text = re.sub(r"[0-9a-zA-Z]{6}", r"\g<0>A0", text)
-
         # 英数字8桁のみを抽出
         numbering = re.findall(r"[0-9a-zA-Z]{8}", text)
 
@@ -38,7 +37,7 @@ def csv(year: str) -> None:
     numbering_dict = dict()
 
     for file in tqdm(files):
-        with open(f"./data/{year}/csv/{file}", "r", encoding="utf-8") as f:
+        with open(f"{path}/{file}", "r", encoding="utf-8") as f:
             # ファイルの中身をコンマ区切りでリストに変換
             numbering = f.read().split(",")
 
